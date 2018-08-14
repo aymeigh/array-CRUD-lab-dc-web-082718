212e267aecdd9cf5b7270943d82ef9f6d20d26d8
Amy Zhou <aymeighz@gmail.com>
2018-08-14 02:24:44 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["red", "yellow", "purple", "pink"]
-  array.last "blue"
+  array.last(element)
   return array
 end
 
